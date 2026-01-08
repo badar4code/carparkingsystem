20c076f5568a8727109b47c692cba74cd9a15149
badar4code <bader5uk@hotmail.com>
2026-01-08 14:35:47 +0400

    added methods to save and search if type exists

--- src/main/java/badar4code/carparkingsystem/repository/VehicleTypeRepo.java
+++ src/main/java/badar4code/carparkingsystem/repository/VehicleTypeRepo.java
@@ -1,7 +1,11 @@
 package badar4code.carparkingsystem.repository;
 
 import badar4code.carparkingsystem.model.entity.VehicleType;
+import badar4code.carparkingsystem.model.record.VehicleTypeDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehicleTypeRepo extends JpaRepository<VehicleType, Long> {
+    public boolean existsVehicleTypesByCode(String code);
 }
--- src/main/java/badar4code/carparkingsystem/service/VehicleTypeService.java
+++ src/main/java/badar4code/carparkingsystem/service/VehicleTypeService.java
@@ -1,6 +1,8 @@
 package badar4code.carparkingsystem.service;
 
 
+import badar4code.carparkingsystem.controller.BaseController;
+import badar4code.carparkingsystem.handler.ApiResponseHandler;
 import badar4code.carparkingsystem.helper.ApiResponse;
 import badar4code.carparkingsystem.helper.GenericMapper;
 import badar4code.carparkingsystem.model.entity.VehicleType;
@@ -8,29 +10,45 @@ import badar4code.carparkingsystem.model.record.VehicleTypeDto;
 import badar4code.carparkingsystem.repository.VehicleTypeRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class VehicleTypeService {
+public class VehicleTypeService extends BaseController {
 
     private final VehicleTypeRepo vehicleTypeRepo;
 
     private final GenericMapper genericMapper;
 
+    private final ApiResponseHandler responseHandler;
+
 
     @Autowired
     public VehicleTypeService(VehicleTypeRepo vehicleTypeRepo,
-                              GenericMapper genericMapper) {
+                              GenericMapper genericMapper, ApiResponseHandler responseHandler) {
         this.vehicleTypeRepo = vehicleTypeRepo;
         this.genericMapper = genericMapper;
+        this.responseHandler = responseHandler;
     }
 
     public ApiResponse<VehicleTypeDto> findById(Long id) {
-        VehicleType vehicleType = vehicleTypeRepo.findById(id).orElse(null);
-        if (vehicleType != null) {
-            VehicleTypeDto dto = genericMapper.map(vehicleType, VehicleTypeDto.class);
-            return new ApiResponse<>("", "successful", dto);
+        Optional<VehicleType> result = vehicleTypeRepo.findById(id);
+
+        if (result.isPresent()) {
+            return responseHandler.success(genericMapper.map(result.get(), VehicleTypeDto.class), "Found");
+        } else {
+            return responseHandler.notFound("Vehicle Type not found");
         }
-        return new ApiResponse<>("", "not found", null);
+    }
+
+    public ApiResponse<VehicleTypeDto> save(VehicleTypeDto dto) {
+        if (vehicleTypeRepo.existsVehicleTypesByCode(dto.code())) {
+            return responseHandler.badRequest("Code already exists!");
+        }
+        VehicleType vehicleType = genericMapper.map(dto, VehicleType.class);
+        vehicleTypeRepo.save(vehicleType);
+        return responseHandler.success(genericMapper.map(vehicleType, VehicleTypeDto.class), "Saved");
     }
 }
